a13757cc9e8f4b99a2e570a9c16c91855e6169fa
James <jameslopez@email.com>
2026-03-07 02:05:02 +0800

    feat: import the nunito font family for changing texts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,23 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 import "../global.css";
+
+SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    "Nunito-Light": require("../assets/fonts/Nunito/Nunito-Light.ttf"),
+    "Nunito-Regular": require("../assets/fonts/Nunito/Nunito-Regular.ttf"),
+    "Nunito-Medium": require("../assets/fonts/Nunito/Nunito-Medium.ttf"),
+    "Nunito-SemiBold": require("../assets/fonts/Nunito/Nunito-SemiBold.ttf"),
+    "Nunito-Bold": require("../assets/fonts/Nunito/Nunito-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) SplashScreen.hideAsync();
+  }, [loaded, error]);
+
+  if (!loaded && !error) return null;
+
   return <Stack screenOptions={{ headerShown: false }} />;
 }
