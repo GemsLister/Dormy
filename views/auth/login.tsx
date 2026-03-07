41d17583bbf73ec65a7d9dd6ec37ad81a141bb93
James <jameslopez@email.com>
2026-03-07 23:52:50 +0800

    style: adjust the font size

--- views/auth/login.tsx
+++ views/auth/login.tsx
@@ -11,37 +11,39 @@ import { Text, View } from "react-native";
 export default function LoginView() {
   return (
     <AuthContainer>
-      {/* Inputs */}
       <AuthForm>
-        <View className="gap-5">
-          {/* Inputs */}
-          <View className="gap-2">
-            <Text className="text-lg font-nunito-bold text-gray-600">
-              Phone no.
-            </Text>
-            <NumberPad />
+        <View className="gap-10">
+          <View className="gap-7">
+            {/* Inputs */}
+            <View className="gap-2">
+              <Text className="font-nunito-bold text-gray-600 text-[20px]">
+                Phone no.
+              </Text>
+              <NumberPad placeholder="Enter Your Phone no." />
+            </View>
+            <View className="gap-2">
+              <Text className="text-lg font-nunito-bold text-gray-600 text-[20px]">
+                Password:
+              </Text>
+              <Password placeholder="Enter Your Password" />
+            </View>
           </View>
-          <View className="gap-2">
-            <Text className="text-lg font-nunito-bold text-gray-600">
-              Password
-            </Text>
-            <Password />
+          {/* Remember Me and Forgot Password */}
+          <View className="flex-row justify-between">
+            <View className="flex-row gap-3">
+              <Checkbox />
+              <Text className="font-nunito-semi-bold text-[18px] text-gray-600">
+                Remember Me
+              </Text>
+            </View>
+            <View className="flex-row">
+              <Text className=" font-nunito-semi-bold text-[18px] text-gray-600">
+                Forgot Password?
+              </Text>
+            </View>
           </View>
         </View>
-        {/* Remember Me and Forgot Password */}
-        <View className="flex-row justify-between">
-          <View className="flex-row gap-3">
-            <Checkbox />
-            <Text className="font-nunito-semi-bold text-[14px] text-gray-600">
-              Remember Me
-            </Text>
-          </View>
-          <View className="flex-row">
-            <Text className=" font-nunito-semi-bold text-[14px] text-gray-600">
-              Forgot Password
-            </Text>
-          </View>
-        </View>
+
         {/* Buttons */}
         <View className="gap-5">
           <PrimaryButton title="Login" />
