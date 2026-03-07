553abe341c7cacc04704b8bd25f631d927848bb6
James <jameslopez@email.com>
2026-03-07 21:27:42 +0800

    added remember me and forgot password

--- views/auth/login.tsx
+++ views/auth/login.tsx
@@ -1,54 +1,55 @@
-import AuthContainer from "@/components/container/AuthContainer";
+import AuthContainer from "@/components/containers/AuthContainer";
 import PrimaryButton from "@/components/ui/button/PrimaryButton";
 import SecondaryButton from "@/components/ui/button/SecondaryButton";
-import { Image } from "expo-image";
+import AuthForm from "@/components/ui/forms/AuthForm";
+import NumberPad from "@/components/ui/inputs/NumberPad";
+import Password from "@/components/ui/inputs/Password";
+import Checkbox from "expo-checkbox";
 import { Link } from "expo-router";
-import { Text, TextInput, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Text, View } from "react-native";
 
 export default function LoginView() {
   return (
     <AuthContainer>
-      <SafeAreaView>
-        <Image
-          source={require("../../assets/dormy-logo-text.png")}
-          style={{ height: 90, width: 140 }}
-          contentFit="contain"
-        />
-      </SafeAreaView>
       {/* Inputs */}
-      <View className="w-[80%] gap-10">
+      <AuthForm>
         <View className="gap-5">
+          {/* Inputs */}
           <View className="gap-2">
             <Text className="text-lg font-nunito-bold text-gray-600">
               Phone no.
             </Text>
-            <TextInput
-              keyboardType="number-pad"
-              className="font-nunito-semi-bold text-lg border-b border-b-gray-400 text-gray-600"
-              placeholder="Enter Your Phone Number"
-              maxLength={11}
-            />
+            <NumberPad />
           </View>
           <View className="gap-2">
             <Text className="text-lg font-nunito-bold text-gray-600">
               Password
             </Text>
-            <TextInput
-              keyboardType="default"
-              className="font-nunito-semi-bold text-lg border-b border-b-gray-400 text-gray-600"
-              placeholder="Enter Your Password"
-              secureTextEntry={true}
-            />
+            <Password />
           </View>
         </View>
+        {/* Remember Me and Forgot Password */}
+        <View className="flex-row justify-between">
+          <View className="flex-row gap-3">
+            <Checkbox />
+            <Text className="font-nunito-semi-bold text-[14px] text-gray-600">
+              Remember Me
+            </Text>
+          </View>
+          <View className="flex-row">
+            <Text className=" font-nunito-semi-bold text-[14px] text-gray-600">
+              Forgot Password
+            </Text>
+          </View>
+        </View>
+        {/* Buttons */}
         <View className="gap-5">
           <PrimaryButton title="Login" />
           <Link href={"/register"} asChild>
             <SecondaryButton title="Register" />
           </Link>
         </View>
-      </View>
+      </AuthForm>
     </AuthContainer>
   );
 }
