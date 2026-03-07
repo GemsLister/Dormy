b9789e4c266e4302a28c6723642792ec8a8399c9
James <jameslopez@email.com>
2026-03-07 23:49:03 +0800

    ref: added register button, back to login screen, and placeholder props for displaying placeholders

--- views/auth/register.tsx
+++ views/auth/register.tsx
@@ -1,7 +1,9 @@
 import AuthContainer from "@/components/containers/AuthContainer";
+import PrimaryButton from "@/components/ui/button/PrimaryButton";
 import AuthForm from "@/components/ui/forms/AuthForm";
 import NumberPad from "@/components/ui/inputs/NumberPad";
 import Password from "@/components/ui/inputs/Password";
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function RegisterView() {
@@ -13,18 +15,35 @@ export default function RegisterView() {
             Register
           </Text>
         </View>
-
-        <View>
-          <Text className="text-[18px] font-nunito-semi-bold text-gray-600">
-            Phone no.
-          </Text>
-          <NumberPad />
-        </View>
-        <View>
-          <Text className="text-[18px] font-nunito-semi-bold text-gray-600">
-            Password
-          </Text>
-          <Password />
+        <View className="gap-7">
+          {/* Inputs */}
+          <View className="gap-7">
+            {/* Phone no. */}
+            <View>
+              <Text className="text-[20px] font-nunito-semi-bold text-gray-600">
+                Phone no.
+              </Text>
+              <NumberPad placeholder="Register Phone no." />
+            </View>
+            {/* Password */}
+            <View>
+              <Text className="text-[20px] font-nunito-semi-bold text-gray-600">
+                Password
+              </Text>
+              <Password placeholder="Create password" />
+            </View>
+          </View>
+          <View className="gap-5">
+            <PrimaryButton title="Sign Up" />
+            {/* Go back to login screen */}
+            <View className="items-center">
+              <Link href={"/login"} asChild>
+                <Text className="font-nunito-semi-bold text-[18px] text-gray-600">
+                  Already have an account?
+                </Text>
+              </Link>
+            </View>
+          </View>
         </View>
       </AuthForm>
     </AuthContainer>
