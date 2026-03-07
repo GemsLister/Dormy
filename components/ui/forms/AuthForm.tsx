2be7a59cb78c2c4f583e428b06b0fe8fba1fc825
James <jameslopez@email.com>
2026-03-07 23:51:37 +0800

    feat: placed children inside the box

--- components/ui/forms/AuthForm.tsx
+++ components/ui/forms/AuthForm.tsx
@@ -2,5 +2,11 @@ import { PropsWithChildren } from "react";
 import { View } from "react-native";
 
 export default function AuthForm({ children }: PropsWithChildren) {
-  return <View className="w-full px-12 gap-10">{children}</View>;
+  return (
+    <View className="w-full px-5">
+      <View className="p-12 gap-8 rounded-[25px] bg-pale-peach shadow-12 shadow-slate-400">
+        {children}
+      </View>
+    </View>
+  );
 }
