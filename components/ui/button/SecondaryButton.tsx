a910a84ec2b2367a6a0e64637a405fb5ba64a2b0
James <jameslopez@email.com>
2026-03-07 20:33:23 +0800

    ref: used props for clean code

--- components/ui/button/SecondaryButton.tsx
+++ components/ui/button/SecondaryButton.tsx
@@ -1,21 +1,18 @@
-import React from "react";
 import { Pressable, Text, View } from "react-native";
 
-export default function PrimaryButton({
-  children,
-  onPress,
-}: {
-  children: React.ReactNode;
-  onPress?: () => void;
-}) {
+type PrimaryButtonProps = {
+  title: string;
+};
+
+export default function PrimaryButton({ title, ...props }: PrimaryButtonProps) {
   return (
     <View>
       <Pressable
-        onPress={onPress}
-        className="border border-charming_orange p-4 items-center active:opacity-80 rounded-full"
+        {...props}
+        className="bg-slate-100 border border-charming_orange p-4 items-center active:opacity-80 rounded-full"
       >
         <Text className="text-charming_orange text-xl font-nunito-bold">
-          {children}
+          {title}
         </Text>
       </Pressable>
     </View>
